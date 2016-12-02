bc950ca48b5819b5b86c0c6cdcf4c0ab228aa714
Kevin Cho <takearest118@gmail.com>
2016-12-02 21:39:06 +0900

    apply request mobile number process

--- src/app/notification.service.ts
+++ src/app/notification.service.ts
@@ -8,13 +8,23 @@ import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class NotificationService {
+  private notificationUrl = 'http://' + environment.api.host + ':' + environment.api.port + '/a/notification';
   private notificationsUrl = 'http://' + environment.api.host + ':' + environment.api.port + '/a/notifications';
 
   constructor(private http: Http) { }
 
   public getNotifications(start:Number, size:Number, _id: string): Observable<{}> {
-    this.notificationsUrl += '?start=' + start + '&size=' + size + '&_id=' + _id;
-    return this.http.get(this.notificationsUrl)
+    let url = this.notificationsUrl + '?start=' + start + '&size=' + size + '&_id=' + _id;
+    return this.http.get(url)
+                    .map((response: Response) => response.json())
+                    .catch((error: any) => Observable.throw(error.json() || 'Server error'));
+  }
+
+  public readNotification(_id: string): Observable<{}> {
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers});
+    let url = this.notificationUrl + '/' + _id;
+    return this.http.put(url, JSON.stringify({read: true}), options)
                     .map((response: Response) => response.json())
                     .catch((error: any) => Observable.throw(error.json() || 'Server error'));
   }
