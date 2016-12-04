3efaa28f7a859fd08e5217a8c6bdb21e3f6c46e1
Kevin Cho <takearest118@gmail.com>
2016-12-04 23:45:11 +0900

    add locale_id provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
@@ -45,6 +45,10 @@ import { NotificationService } from './notification.service';
     ])
   ],
   providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'en-US'
+    },
     AuthGuard,
     AuthService,
     InvitationService,
