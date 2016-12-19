7dc6d8335ce6b77769b728d67b9ee3e837e88441
Kevin Cho <takearest118@gmail.com>
2016-12-19 10:47:41 +0900

    add group and type on home

--- src/app/data-filter.pipe.ts
+++ src/app/data-filter.pipe.ts
@@ -8,7 +8,10 @@ export class DataFilterPipe implements PipeTransform {
   transform(array: any[], query: string): any {
     if(query) {
       return array.filter(row => {
-        if(row.name.indexOf(query) > -1 || row.mobile_number.indexOf(query) > -1) {
+        if(row.name.indexOf(query) > -1 ||
+          row.group.indexOf(query) > -1 ||
+          row.type.indexOf(query) > -1 ||
+          row.mobile_number.indexOf(query) > -1) {
           return true;
         }
       });
