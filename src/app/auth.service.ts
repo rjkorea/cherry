d8d9da98c2c61183ed7bfebc5a11eab75b3c9fe1
Kevin Cho <takearest118@gmail.com>
2017-01-17 16:53:11 +0900

    fix using cookie

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import { CookieService } from 'angular2-cookie/core';
 import { Observable } from 'rxjs/Rx';
 import { environment } from '../environments/environment';
 
@@ -11,18 +12,18 @@ export class AuthService {
   private loginUrl = 'http://' + environment.api.host + ':' + environment.api.port + '/a/auth/login';
   private isLoggedIn = false;
 
-  constructor(private http: Http) {
-    this.isLoggedIn = !!localStorage.getItem('csk');
+  constructor(private http: Http, private cookieService: CookieService) {
+    this.isLoggedIn = !!this.cookieService.get('csk');
   }
 
   public login(email: string, password: string): Observable<{}> {
     let headers = new Headers({'Content-Type': 'application/json'});
-    let options = new RequestOptions({headers: headers});
+    let options = new RequestOptions({headers: headers, withCredentials: true});
     return this.http.post(this.loginUrl, JSON.stringify({email, password}), options)
                     .map((response: Response) => {
                       if(response.status==200) {
                         console.log(response);
-                        localStorage.setItem('csk', response.json().data.csk);
+                        console.log(this.cookieService.get('csk'));
                         localStorage.setItem('name', response.json().data.admin.name);
                         localStorage.setItem('_id', response.json().data.admin._id);
                         localStorage.setItem('image', response.json().data.admin.image);
@@ -34,7 +35,7 @@ export class AuthService {
   }
 
   logout() {
-    localStorage.removeItem('csk');
+    this.cookieService.remove('csk');
     localStorage.removeItem('name');
     localStorage.removeItem('_id');
     localStorage.removeItem('image');
