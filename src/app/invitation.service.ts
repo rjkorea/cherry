ff7888148ca073b1ddd905f179875e3a5458b27c
Kevin Cho <takearest118@gmail.com>
2017-01-19 16:12:47 +0900

    apply auth of admin

--- src/app/invitation.service.ts
+++ src/app/invitation.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
+import { CookieService } from 'angular2-cookie/core';
 import { environment } from '../environments/environment';
 
 import 'rxjs/add/operator/map';
@@ -10,39 +11,39 @@ import 'rxjs/add/operator/catch';
 export class InvitationService {
   private invitationsUrl = 'http://' + environment.api.host + ':' + environment.api.port + '/a/invitations';
   private invitationUrl = 'http://' + environment.api.host + ':' + environment.api.port + '/a/invitation';
+  private options;
 
-  constructor(private http: Http) { }
+  constructor(private http: Http, private cookieService: CookieService) {
+    let headers = new Headers({'Content-Type': 'application/json', 'Set-Cookie': 'csk=' + this.cookieService.get('csk')});
+    this.options = new RequestOptions({headers: headers, withCredentials: true});
+  }
 
   public getInvitations(start: Number, size: Number): Observable<{}> {
     let url = this.invitationsUrl + '?start=' + start + '&size=' + size;
-    // let headers = new Headers({'Cookie': 'csk=' + localStorage.getItem('csk')});
-    // let options = new RequestOptions({headers: headers, withCredentials: true});
-    return this.http.get(url)
+    return this.http.get(url, this.options)
                     .map((response: Response) => response.json())
                     .catch((error: any) => Observable.throw(error.json() || 'Server error'));
   }
 
   public getInvitation(_id: string): Observable<{}> {
     let url = this.invitationUrl + '/' + _id;
-    return this.http.get(url)
+    return this.http.get(url, this.options)
                     .map((response: Response) => response.json())
                     .catch((error: any) => Observable.throw(error.json() || 'Server error'));
   }
 
   public addInvitation(invitation: any): Observable<{}> {
-    return this.http.post(this.invitationUrl, JSON.stringify(invitation))
+    return this.http.post(this.invitationUrl, JSON.stringify(invitation), this.options)
                     .map((response: Response) => response.json())
                     .catch((error: any) => Observable.throw(error.json() || 'Server error'));
   }
 
   public updateInvitation(invitation: any): Observable<{}> {
-    let headers = new Headers({'Content-Type': 'application/json'});
-    let options = new RequestOptions({headers: headers});
     let url = this.invitationUrl + '/' + invitation['_id']
     delete invitation['_id'];
     delete invitation['updated_at'];
     delete invitation['created_at'];
-    return this.http.put(url, JSON.stringify(invitation), options)
+    return this.http.put(url, JSON.stringify(invitation), this.options)
                     .map((response: Response) => response.json())
                     .catch((error: any) => Observable.throw(error.json() || 'Server error'));
   }
