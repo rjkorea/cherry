f53517d01162ccd243d5c3f2d4d365578ed9bcfb
Kevin Cho <takearest118@gmail.com>
2017-02-16 16:59:26 +0900

    fixed NotificationsService on LoginComponent

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,8 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [NotificationsService]
 })
 export class LoginComponent implements OnInit {
   model: any = {};
