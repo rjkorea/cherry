2692ae4d0622ac7a4aa5adc576dbcf5925676dad
Kevin Cho <takearest118@gmail.com>
2017-03-08 18:33:34 +0900

    add signup for host

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -10,6 +10,7 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 export class AuthService {
   private loginUrl = 'http://' + environment.api.host + ':' + environment.api.port + '/a/auth/login';
+  private signupUrl = 'http://' + environment.api.host + ':' + environment.api.port + '/a/auth/register';
   private isLoggedIn = false;
 
   constructor(private http: Http, private cookieService: CookieService) {
@@ -44,6 +45,20 @@ export class AuthService {
     localStorage.removeItem('_id');
     localStorage.removeItem('image');
     this.isLoggedIn = false;
+  }
+
+  public signup(name: string, email: string, password: string,
+                password2: string, mobile_number: string): Observable<{}> {
+    let headers = new Headers({'Content-Type': 'application/json'});
+    let options = new RequestOptions({headers: headers, withCredentials: true});
+    let body = JSON.stringify({name, email, password, password2, mobile_number, 'role':['host']});
+    return this.http.post(this.signupUrl, body, options)
+                    .map((response: Response) => {
+                      if(response.status==200) {
+                        console.log(response);
+                      }
+                    })
+                    .catch((error: any) => Observable.throw(error.json() || 'Server error'));
   }
 
 }
