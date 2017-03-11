12cb6bc5934a6d128b30ab0c26f209083dfcc3a2
Kevin Cho <takearest118@gmail.com>
2017-03-11 18:24:28 +0900

    add paging on user menu

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params} from '@angular/router';
 import { UserService } from '../user.service';
 import { NotificationsService } from 'angular2-notifications';
 
@@ -11,12 +12,20 @@ import { NotificationsService } from 'angular2-notifications';
 export class UserComponent implements OnInit {
   private admins: Array<Object>;
   private notification_options: Object;
+  private page: any = 1;
+  private size: any = 2;
 
   constructor(private userService: UserService,
+              private route: ActivatedRoute,
+              private router: Router,
               private simpleNotificationsService: NotificationsService) { }
 
   ngOnInit() {
-    this.loadAdmins();
+    let params: Params = this.route.snapshot.params;
+    if('page' in params) {
+      this.page = parseInt(params['page']);
+    }
+    this.loadAdmins(this.page);
     this.notification_options = {
       timeOut: 3000,
       showProgressBar: true,
@@ -26,8 +35,8 @@ export class UserComponent implements OnInit {
     }
   }
 
-  loadAdmins() {
-    this.userService.getAdmins(0, 10)
+  loadAdmins(page: any) {
+    this.userService.getAdmins((page-1)*this.size, this.size)
       .subscribe(
         response => {
           this.admins = response['data'];
