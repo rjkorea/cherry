110e9963225667adba6d187fb06dfe0f4e1078b2
Kevin Cho <takearest118@gmail.com>
2017-05-25 19:40:12 +0900

    remove notification on new content

--- src/app/components/content/content-new.component.ts
+++ src/app/components/content/content-new.component.ts
@@ -1,34 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ContentService } from '../../services/content.service';
-import { NotificationsService } from 'angular2-notifications';
 
 @Component({
   selector: 'app-content-new',
   templateUrl: './content-new.component.html',
   styleUrls: ['./content-new.component.css'],
-  providers: [NotificationsService]
+  providers: []
 })
 export class ContentNewComponent implements OnInit {
   content: any;
-  notification_options: Object;
 
   constructor(private contentService: ContentService,
-              private router: Router,
-              private simpleNotificationsService: NotificationsService) { }
+              private router: Router) { }
 
   ngOnInit() {
     this.content = {
       name: '',
       place: '',
       desc: ''
-    }
-    this.notification_options = {
-      timeOut: 3000,
-      showProgressBar: true,
-      pauseOnHover: false,
-      clickToClose: true,
-      maxLength: 128
     }
   }
 
@@ -39,11 +29,6 @@ export class ContentNewComponent implements OnInit {
           this.router.navigate(['/content']);
         },
         error => {
-          this.simpleNotificationsService.error(
-            'Error',
-            error['message'],
-            this.notification_options
-          );
           console.log(error);
         }
       );
