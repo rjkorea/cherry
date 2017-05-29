3d1063ea31133a998db043e3061b881019f7bff8
Kevin Cho <takearest118@gmail.com>
2017-05-29 19:26:22 +0900

    update new comp for ticket register

--- src/app/components/ticket/ticket-register.component.ts
+++ src/app/components/ticket/ticket-register.component.ts
@@ -10,6 +10,9 @@ import { TicketService } from '../../services/ticket.service';
 })
 export class TicketRegisterComponent implements OnInit {
   private user: any;
+  private genders: any;
+  private country_codes: any;
+  private country_code: string;
 
   constructor(private ticketService: TicketService,
               private route: ActivatedRoute,
@@ -22,10 +25,29 @@ export class TicketRegisterComponent implements OnInit {
       birthday: '',
       gender: '',
       email: ''
-    }
+    };
+    this.genders = [
+      {text: 'Male', value: 'male'},
+      {text: 'Female', value: 'female'}
+    ];
+    this.country_codes = [
+      {value: '+82', text: 'South Korea (+82)'},
+      {value: '+61', text: 'Australia (+61)'},
+      {value: '+55', text: 'Brazil (+55)'},
+      {value: '+1', text: 'Canada (+1)'},
+      {value: '+86', text: 'China (+86)'},
+      {value: '+49', text: 'Germany (+49)'},
+      {value: '+852', text: 'Hong Kong (+852)'},
+      {value: '+81', text: 'Japan (+81)'},
+      {value: '+65', text: 'Singapore (+65)'},
+      {value: '+66', text: 'Thailand (+66)'},
+      {value: '+44', text: 'United Kingdom (+44)'},
+      {value: '+1', text: 'United States (+1)'}
+    ];
   }
 
   private onDone() {
+    this.user.mobile_number = this.country_code.slice(1) + this.user.mobile_number.slice(1);
     this.ticketService.registerTicket(this.route.snapshot.params['id'], this.user)
       .subscribe(
         response => {
@@ -38,7 +60,7 @@ export class TicketRegisterComponent implements OnInit {
   }
 
   private disabledDone() {
-    return !(this.user.name && this.user.mobile_number && this.user.gender && this.user.birthday && this.user.email);
+    return !(this.user.name && this.user.mobile_number && this.user.gender && this.user.birthday);
   }
 
 }
