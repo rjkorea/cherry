6ef12d0f27a0e7121dc6253de4beba2322afca54
Kevin Cho <takearest118@gmail.com>
2017-06-12 16:08:20 +0900

    add open graph for host application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private meta: Meta) {
+    this.meta.addTag({property: 'og:title', content: '세상의 모든 즐거움을 티킷하다'});
+    this.meta.addTag({property: 'og:image', content: 'http://www.tkit.me/images/opengraph.jpg'});
+    this.meta.addTag({property: 'og:url', content: 'http://host.tkit.me'});
+    this.meta.addTag({property: 'og.type', content: 'website'});
+  }
 }
