5ebb188a3975c775af6b1548ce961e635086a94c
Kevin Cho <takearest118@gmail.com>
2017-06-12 16:43:08 +0900

    fix dot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,6 @@ export class AppComponent {
     this.meta.addTag({property: 'og:title', content: '세상의 모든 즐거움을 티킷하다'});
     this.meta.addTag({property: 'og:image', content: 'http://www.tkit.me/images/opengraph.jpg'});
     this.meta.addTag({property: 'og:url', content: 'http://host.tkit.me'});
-    this.meta.addTag({property: 'og.type', content: 'website'});
+    this.meta.addTag({property: 'og:type', content: 'website'});
   }
 }
