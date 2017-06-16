143dc6d8bbf68d5b5d84fb0531337571efd5a2ff
Kevin Cho <takearest118@gmail.com>
2017-06-16 19:07:25 +0900

    add country list service

---
+++ src/app/services/util.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { environment } from '../../environments/environment';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+const URL = `${environment.api.protocol}://${environment.api.host}:${environment.api.port}`;
+
+@Injectable()
+export class UtilService {
+  private countriesUrl = `${URL}/a/countries`;
+  private options;
+
+  constructor(private http: Http) {
+    const headers = new Headers({'Content-Type': 'application/json', 'Authorization': 'csk=' + localStorage.getItem('csk')});
+    this.options = new RequestOptions({headers: headers});
+  }
+
+  public getCountryList(query: string, start: Number, size: Number): Observable<{}> {
+    const url = `${this.countriesUrl}?q=${query}&start=${start}&size=${size}`;
+    return this.http.get(url, this.options)
+                    .map((response: Response) => response.json())
+                    .catch((error: any) => Observable.throw(error.json() || 'Server error'));
+  }
+
+}
