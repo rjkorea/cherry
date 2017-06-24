0719409ae8cab2c9d8bebd09760b889d49a6f7de
Kevin Cho <takearest118@gmail.com>
2017-06-24 11:42:12 +0900

    add loading on company

--- src/app/components/company/company-list.component.ts
+++ src/app/components/company/company-list.component.ts
@@ -9,33 +9,38 @@ import { CompanyService } from '../../services/company.service';
   providers: []
 })
 export class CompanyListComponent implements OnInit {
-  companies: Array<Object>;
+  companies: Array<any>;
   query: any = '';
   page: any = 1;
   size: any = 9;
   count: any = 0;
+
+  is_loading: boolean;
 
   constructor(private companyService: CompanyService,
               private route: ActivatedRoute,
               private router: Router) { }
 
   ngOnInit() {
+    this.is_loading = true;
     const params: Params = this.route.snapshot.params;
-    if('query'in params) {
+    if ('query' in params) {
       this.query = params['query'];
     }
-    if('page' in params) {
+    if ('page' in params) {
       this.page = +params['page'];
     }
     this.loadCompanies(this.query, this.page);
   }
 
-  loadCompanies(query:any, page: any) {
-    this.companyService.getCompanyList(query, (page-1)*this.size, this.size)
+  loadCompanies(query: any, page: any) {
+    this.is_loading = true;
+    this.companyService.getCompanyList(query, (page - 1) * this.size, this.size)
       .subscribe(
         response => {
           this.count = response['count'];
           this.companies = response['data'];
+          this.is_loading = false;
         },
         error => {
           console.log(error);
@@ -45,7 +50,7 @@ export class CompanyListComponent implements OnInit {
 
   onPrev() {
     const page = this.page - 1;
-    this.page = page
+    this.page = page;
     this.router.navigate(['/company', {query: this.query, page: page}]);
     this.loadCompanies(this.query, page);
   }
