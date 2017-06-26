2f3630f3e01c902b965784c3b84c9f52ed594740
Kevin Cho <takearest118@gmail.com>
2017-06-26 10:37:58 +0900

    add entrance-loader

---
+++ src/app/components/shared/entrance-loader/entrance-loader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entrance-loader',
+  templateUrl: './entrance-loader.component.html',
+  styleUrls: ['./entrance-loader.component.css']
+})
+export class EntranceLoaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/components/shared/shared-components.module.ts
+++ src/app/components/shared/shared-components.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LoaderComponent } from './loader/loader.component';
 import { ErrorMessageComponent } from './error-message/error-message.component';
+import { EntranceLoaderComponent } from './entrance-loader/entrance-loader.component';
+
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ LoaderComponent, ErrorMessageComponent ],
-  exports: [ LoaderComponent, ErrorMessageComponent ]
+  declarations: [ LoaderComponent, ErrorMessageComponent, EntranceLoaderComponent ],
+  exports: [ LoaderComponent, ErrorMessageComponent, EntranceLoaderComponent ]
 })
 export class SharedComponentsModule {}
