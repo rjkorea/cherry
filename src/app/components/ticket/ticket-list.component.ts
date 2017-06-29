1b6984ba2751e08d6aaf3e623ec19ec880dfd01d
Kevin Cho <takearest118@gmail.com>
2017-06-29 17:34:59 +0900

    update table ui on list of ticket

--- src/app/components/ticket/ticket-list.component.ts
+++ src/app/components/ticket/ticket-list.component.ts
@@ -21,6 +21,7 @@ export class TicketListComponent implements OnInit {
   companies: any;
   content_oid: string = '';
   contents: any;
+  is_loading: boolean;
 
   constructor(private ticketService: TicketService,
               private authService: AuthService,
@@ -30,6 +31,7 @@ export class TicketListComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit() {
+    this.is_loading = true;
     this.companies = [
       {
         _id: '',
@@ -46,25 +48,28 @@ export class TicketListComponent implements OnInit {
       }
     ];
     this.loadContents();
-    let params: Params = this.route.snapshot.params;
-    if('query'in params) {
+    const params: Params = this.route.snapshot.params;
+    if ('query'in params) {
       this.query = params['query'];
     }
-    if('page' in params) {
+    if ('page' in params) {
       this.page = +params['page'];
     }
     this.loadTickets(this.query, this.page);
   }
 
   loadTickets(query:any, page: any) {
+    this.is_loading = true;
     this.ticketService.getTicketList(query, this.company_oid, this.content_oid, (page-1)*this.size, this.size)
       .subscribe(
         response => {
           this.count = response['count'];
           this.tickets = response['data'];
           window.scrollTo(0, 0);
+          this.is_loading = false;
         },
         error => {
+          this.is_loading = false;
           console.log(error);
         }
       );
