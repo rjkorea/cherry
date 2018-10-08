2946cb8495bdca88329e2cfaecb84146c881c183
Kevin Cho <takearest118@gmail.com>
2018-10-08 17:29:56 +0900

    remove kpi on dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import { DashboardService } from '../../services/dashboard.service';
+import { AuthService } from '../../services/auth.service';
 import { ContentService } from '../../services/content.service';
-import { CloudData, CloudOptions } from 'angular-tag-cloud-module';
 
 @Component({
   selector: 'app-dashboard',
@@ -20,27 +20,18 @@ export class DashboardComponent implements OnInit {
   total_group_ticket_count: number;
   total_content_count: number;
   total_gender_chart: any;
-  top_contents;
 
   ticket_count: any;
-  avg_age: any;
   revenue: any;
-  pre_revenue: any;
-  top_ticket_types;
-  top_ticket_orders;
 
-  register_gender_chart: any;
-  use_gender_chart: any;
-  age_chart: any;
   ticket_count_chart: any;
-
-  content_cloud: any;
 
   is_loading: boolean;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
               private dashboardService: DashboardService,
+              private authService: AuthService,
               private contentService: ContentService) { }
 
   ngOnInit() {
@@ -85,70 +76,13 @@ export class DashboardComponent implements OnInit {
         maintainAspectRatio: true
       }
     };
-    this.top_contents = [];
-    this.top_ticket_types = [];
-    this.top_ticket_orders = [];
     this.ticket_count = {
       use: 0,
       total: 0
     };
-    this.avg_age = {
-      female: 0,
-      gender: 0
-    };
     this.revenue = {
       cash: 0,
       creditcard: 0
-    };
-    this.pre_revenue = {
-      amount: 0,
-      count: 0
-    };
-    this.register_gender_chart = {
-      type: 'doughnut',
-      data: {
-        labels: ['여자', '남자'],
-        datasets: [
-          {
-            data: [0, 0],
-            backgroundColor: [
-              '#FF6384',
-              '#36A2EB'
-            ],
-            hoverBackgroundColor: [
-              '#FF6384',
-              '#36A2EB'
-            ]
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: true
-      }
-    };
-    this.use_gender_chart = {
-      type: 'pie',
-      data: {
-        labels: ['여자', '남자'],
-        datasets: [
-          {
-            data: [0, 0],
-            backgroundColor: [
-              '#FF6384',
-              '#36A2EB'
-            ],
-            hoverBackgroundColor: [
-              '#FF6384',
-              '#36A2EB'
-            ]
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: true
-      }
     };
     this.ticket_count_chart = {
       type: 'pie',
@@ -181,13 +115,6 @@ export class DashboardComponent implements OnInit {
         maintainAspectRatio: true
       }
     };
-    this.content_cloud = {
-      data: [],
-      options: {
-        width: 1,
-        overflow: true
-      }
-    };
 
     this.loadContents();
     const params: Params = this.route.snapshot.params;
@@ -217,11 +144,6 @@ export class DashboardComponent implements OnInit {
           this.total_company_count = response['data']['total_company_count'];
           this.total_user_count = response['data']['total_user_count'];
           this.total_content_count = response['data']['total_content_count'];
-          this.top_contents = response['data']['top_contents'];
-          this.content_cloud.data = [];
-          for (const c of this.top_contents) {
-            this.content_cloud.data.push({text: c.content.name, weight: c.ticket_cnt});
-          }
           this.total_gender_chart['data']['datasets'][0]['data'] = [
             response['data']['gender_count']['female'],
             response['data']['gender_count']['male']
@@ -248,19 +170,7 @@ export class DashboardComponent implements OnInit {
             response['data']['ticket_count']['use'],
             response['data']['ticket_count']['cancel']
           ];
-          this.avg_age = response['data']['avg_age'];
           this.revenue = response['data']['revenue'];
-          this.pre_revenue = response['data']['pre_revenue'];
-          this.top_ticket_types = response['data']['top_ticket_types'];
-          this.top_ticket_orders = response['data']['top_ticket_orders'];
-          this.register_gender_chart['data']['datasets'][0]['data'] = [
-            response['data']['gender_count']['register']['female'],
-            response['data']['gender_count']['register']['male']
-          ];
-          this.use_gender_chart['data']['datasets'][0]['data'] = [
-            response['data']['gender_count']['use']['female'],
-            response['data']['gender_count']['use']['male']
-          ];
           this.is_loading = false;
         },
         error => {
