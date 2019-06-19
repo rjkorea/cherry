b464d8089bf2818c4e0a4f77fe2198be63089f12
Kevin Cho <takearest118@gmail.com>
2019-06-19 15:11:52 +0900

    add duplicate ticket type for admin user

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,8 +16,8 @@ export const environment = {
   },
   api: {
     protocol: 'http',
-    // host: 'localhost',
-    host: '192.168.35.183',
+    host: 'localhost',
+    // host: '192.168.35.183',
     // host: '172.16.6.19',
     // host: '172.30.1.20',
     port: 5100
