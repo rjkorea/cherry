27341974d726ba8d7584b5f3817c3d67fd55d378
Kevin Cho <takearest118@gmail.com>
2019-08-01 18:55:38 +0900

    add target service

---
+++ src/app/services/target.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { environment } from '../../environments/environment';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+const URL = `${environment.api.protocol}://${environment.api.host}:${environment.api.port}`;
+
+@Injectable()
+export class TargetService {
+  targetListUrl = `${URL}/a/v2/targets`;
+  targetCountUrl = `${URL}/a/v2/target/count`;
+  targetSendUrl = `${URL}/a/v2/target/send`;
+  options: any;
+
+  constructor(private http: Http) {
+    const headers = new Headers({'Content-Type': 'application/json', 'Authorization': 'csk=' + localStorage.getItem('csk')});
+    this.options = new RequestOptions({headers: headers, withCredentials: true});
+  }
+
+  public getTargetList(start: number, size: number): Observable<{}> {
+    const url = `${this.targetListUrl}?start=${start}&size=${size}`;
+    return this.http.get(url, this.options)
+                    .map((response: Response) => response.json())
+                    .catch((error: any) => Observable.throw(error.json() || 'Server error'));
+  }
+
+  public getTargetCount(content_oid: string, ticket_type_oids: string, status: string): Observable<{}> {
+    const url = `${this.targetCountUrl}?content_oid=${content_oid}&ticket_type_oids=${ticket_type_oids}&status=${status}`;
+    return this.http.get(url, this.options)
+                    .map((response: Response) => response.json())
+                    .catch((error: any) => Observable.throw(error.json() || 'Server error'));
+  }
+
+  public sendTarget(payload: any): Observable<{}> {
+    const url = `${this.targetSendUrl}`;
+    return this.http.put(url, JSON.stringify(payload), this.options)
+                    .map((response: Response) => response.json())
+                    .catch((error: any) => Observable.throw(error.json() || 'Server error'));
+  }
+
+}
