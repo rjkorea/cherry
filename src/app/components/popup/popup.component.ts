76317312c0e4ae7057340847b25429b075bb6c25
Kevin Cho <takearest118@gmail.com>
2019-08-09 19:40:00 +0900

    add WIP pop component

---
+++ src/app/components/popup/popup.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-popup',
+  templateUrl: './popup.component.html',
+  styleUrls: ['./popup.component.css']
+})
+export class PopupComponent implements OnInit {
+  @Input() title: string;
+  @Input() message: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  done() {
+  }
+
+}
