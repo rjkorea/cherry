c4abcd9d29c9b20f663133c44841daeeec2d75ea
Kevin Cho <takearest118@gmail.com>
2019-08-19 17:43:51 +0900

    update to reset password for host user

--- src/app/components/admin/admin-password.component.ts
+++ src/app/components/admin/admin-password.component.ts
@@ -19,7 +19,6 @@ export class AdminPasswordComponent implements OnInit {
 
   ngOnInit() {
     this.admin = {
-      old_password: '',
       new_password_1: '',
       new_password_2: '',
     }
@@ -32,16 +31,20 @@ export class AdminPasswordComponent implements OnInit {
           this.router.navigate(['/admin', this.route.snapshot.params['id']]);
         },
         error => {
-          this.error_msg = error['message']
+          if (error.message === '400: new password 1 and 2 not matched') {
+            this.error_msg = '비밀번호가 일치하지 않습니다. 다시 입력해주세요.';
+          } else if (error.message === '400: invalid new_password_1' || error.message === '400: invalid new_password_2') {
+            this.error_msg = '비밀번호 형식이 올바르지 않습니다 (8자리이상, 영어 대/소문자, 숫자, 특수기호 포함)';
+          } else {
+            this.error_msg = '서버통신 장애가 발생 하였습니다';
+          }
           console.log(this.error_msg);
         }
       );
   }
 
   disabledSubmit() {
-    return !(this.admin.old_password &&
-            this.admin.new_password_1 &&
-            this.admin.new_password_2);
+    return !(this.admin.new_password_1 && this.admin.new_password_2);
   }
 
 }
